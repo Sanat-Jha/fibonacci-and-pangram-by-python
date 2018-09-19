6381fdd8f670d2f291ee2331a255ef3bd4e34169
aaryantyagi182 <42247648+aaryantyagi182@users.noreply.github.com>
2018-09-19 23:23:29 +0530

    Add files via upload

---
+++ fabonica.py
@@ -0,0 +1,12 @@
+nterms=int(input('no. greater than 0:'))
+
+n1=0
+n2=1
+count=0
+while nterms>count:
+              print(n1)
+              nth=n1+n2
+              n1=n2
+              n2=nth
+              count += 1
+
